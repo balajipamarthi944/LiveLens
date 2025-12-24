da86e786b7410e0505fb63626af9d3d58c6f13a6
Balaji <balupamarthi944@gmail.com>
2025-12-24 17:06:38 +0530

    Fix admin routing for Render

--- backend/livelens/settings.py
+++ backend/livelens/settings.py
@@ -75,8 +75,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'livelens.wsgi.application'
+ROOT_URLCONF = 'livelens.urls'
 
+WSGI_APPLICATION = 'livelens.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
