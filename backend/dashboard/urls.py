612506fb1e6d0217ac2ec99c9c2cb6046e31fbf1
Balaji <balupamarthi944@gmail.com>
2026-02-02 20:52:33 +0530

    initial commit-LiveLens dashboard working

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home
+from .views import dashboard_home,dashboard_stats
 urlpatterns = [
-    path('',home,name='home'),
+    path('home/',dashboard_home,name='dashboard-home'),
+    path('api/stats/',dashboard_stats,name='dashboard-stats'),
 ]
